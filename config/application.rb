e02efa0bc2446acf94610cc09985ab48a464df7d
Andrew McMillion <amcmillion3@gmail.com>
2023-02-26 12:14:27 -0500

    Finished Chapter 2

---
+++ config/application.rb
@@ -0,0 +1,10 @@
+require "rulers"
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "public")
+require "quotes_controller"
+require "home_controller"
+
+module BestQuotes
+  class Application < Rulers::Application
+  end
+end
